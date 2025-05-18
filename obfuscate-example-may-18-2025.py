46cb3d007c5ad70f5f561d4f42aebcd2cb0186f1
Josh Stroud <josh@superaffective.ai>
2025-05-18 15:03:26 -0700

    Obfuscate code example

---
+++ obfuscate-example-may-18-2025.py
@@ -0,0 +1,29 @@
+# Before obfuscation
+
+def hello_world():
+    print("Hello World")
+
+def goodbye_world():
+    print("Goodbye World")
+
+def start_world():
+    print("Start World")
+
+start_world()
+hello_world()
+goodbye_world()
+
+# After obfuscation
+
+def sldfjiasdjfasdfjdsa():
+    print("Hello World")
+
+def asdfiojasfjafsjdf():
+    print("Goodbye World")
+
+def alksdfjlkasdfklj():
+    print("Start World")
+
+sldfjiasdjfasdfjdsa()
+asdfiojasfjafsjdf()
+alksdfjlkasdfklj()
